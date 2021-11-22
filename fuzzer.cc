9b30381ea3d7c694454ae52b9b4fdf0f718c117f
Johannes Riecken <johannes.riecken@gmail.com>
2021-11-22 21:42:07 +0100

    Add an unfuzzed unit test to fuzzing function to get the skeleton working

--- fuzzer.cc
+++ fuzzer.cc
@@ -1,11 +1,12 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#include <iostream>
 
 #include "pcre2_ast.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  string s = "[1,2,3]";
+  string s = "[1, 2, 3]";
   if (to_json(from_json(s)) != s)
     __builtin_trap();
   return 0;
