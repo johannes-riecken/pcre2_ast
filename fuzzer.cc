87f4151591b5ea22acec1cf08565a12851b69761
Johannes Riecken <johannes.riecken@gmail.com>
2022-06-02 21:27:07 +0200

    Set the real fuzzer free

--- fuzzer.cc
+++ fuzzer.cc
@@ -6,13 +6,6 @@
 #include "pcre2_ast.hpp"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  string s = "[1, 2, 3]";
-  if (to_json(from_json(s)) != s)
-    __builtin_trap();
-  return 0;
-}
-
-extern "C" int FuzzOneInput(const uint8_t *data, size_t size) {
   string s( reinterpret_cast<char const*>(data), size ) ;
   if (to_json(from_json(s)) != s)
     __builtin_trap();
