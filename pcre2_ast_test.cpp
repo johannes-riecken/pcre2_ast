b81d4b263bd0103dc50ae7580ecc5280e423ba7b
Johannes Riecken <johannes.riecken@gmail.com>
2022-10-04 21:57:57 +0200

    Add unit tests for simple cases

--- pcre2_ast_test.cpp
+++ pcre2_ast_test.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "pcre2_ast.hpp"
+#include <utility>
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc ,argv);
@@ -8,4 +9,34 @@ int main(int argc, char **argv) {
 
 TEST(Pcre2AstTest, empty) {
   ASSERT_EQ(from_json(""s), nullptr);
+}
+
+TEST(Pcre2AstTest, number) {
+  ASSERT_EQ(*from_json("42"s), JsonValue{42.0});
+}
+
+TEST(Pcre2AstTest, string) {
+  ASSERT_EQ(*from_json("\"foo\""s), JsonValue{"foo"s});
+}
+
+/* TEST(Pcre2AstTest, map) { */
+/*   ASSERT_EQ(*from_json("{\"foo\":42}"s), */
+/*       std::map<std::string, std::shared_ptr<s>>{{"foo"s, std::make_shared<s>(s{JsonValue{42}})}}); */
+/* } */
+
+/* TEST(Pcre2AstTest, array) { */
+/*   ASSERT_EQ(*from_json("[1,2,3]"s), (JsonValue{std::vector<std::shared_ptr<s>>{ */
+/*       { */
+/*       make_shared<s>(s{.v = JsonValue{1.0}}), */
+/*       make_shared<s>(s{.v = JsonValue{2.0}}), */
+/*       make_shared<s>(s{.v = JsonValue{3.0}}), */
+/*       } }})); */
+/* } */
+
+TEST(Pcre2AstTest, bool) {
+  ASSERT_EQ(*from_json("true"s), JsonValue{true});
+}
+
+TEST(Pcre2AstTest, null) {
+  ASSERT_EQ(*from_json("null"s), JsonValue{std::monostate{}});
 }
