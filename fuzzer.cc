1f3322c46a5b22a778fce374ea9f0dc4e5084df5
Johannes Riecken <johannes.riecken@gmail.com>
2022-10-22 17:08:26 +0200

    Use saner fuzzing test

--- fuzzer.cc
+++ fuzzer.cc
@@ -8,14 +8,17 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   string s(reinterpret_cast<char const *>(data), size);
-  auto v = from_json(s);
-  if (v != nullptr) {
-    if (std::holds_alternative<double>(*v) &&
-        std::get<double>(*v) == std::get<double>(*v) - 1.0) {
+  auto v0 = from_json(s);
+  if (v0 != nullptr) {
+    if (std::holds_alternative<double>(*v0) &&
+        std::get<double>(*v0) == std::get<double>(*v0) - 1.0) {
       return 0;
     }
-    if (to_json(std::move(v)) != s)
+    auto v1 = to_json(std::move(v0));
+    auto v2 = from_json(v1);
+    if (((v0 == nullptr) ^ (v2 == nullptr)) || *v0 != *v2) {
       __builtin_trap();
+    }
   }
 
   return 0;
