5784b71e29121000d730ada1d30515dc9e5f16db
Johannes Riecken <johannes.riecken@gmail.com>
2022-10-22 14:23:01 +0200

    Skip fuzzing on inexactly large doubles

--- fuzzer.cc
+++ fuzzer.cc
@@ -1,17 +1,22 @@
-#include <stdint.h>
-#include <stddef.h>
-#include <string.h>
 #include <iostream>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include <variant>
 
 #include "pcre2_ast.hpp"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  string s( reinterpret_cast<char const*>(data), size ) ;
-    auto v = from_json(s);
-    if (v != nullptr) {
-        if (to_json(std::move(v)) != s)
-            __builtin_trap();
+  string s(reinterpret_cast<char const *>(data), size);
+  auto v = from_json(s);
+  if (v != nullptr) {
+    if (std::holds_alternative<double>(*v) &&
+        std::get<double>(*v) == std::get<double>(*v) - 1.0) {
+      return 0;
     }
+    if (to_json(std::move(v)) != s)
+      __builtin_trap();
+  }
 
-    return 0;
+  return 0;
 }
